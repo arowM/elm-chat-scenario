d2748fc7ad5242c75e4091b93f1a1e5ee305bfd4
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-11-19 22:00:27 +0900

    Updates for elm-1.8

--- src/Scenario.elm
+++ src/Scenario.elm
@@ -1,12 +1,13 @@
-module Scenario exposing
-  ( Scenario
-  , succeed
-  , andThen
-  , andAlways
-  , map
-  , print
-  , read
-  )
+module Scenario
+    exposing
+        ( Scenario
+        , succeed
+        , andThen
+        , andAlways
+        , map
+        , print
+        , read
+        )
 
 {-| A type safe DSL for CLI or Conversational User Interface.
 
@@ -28,13 +29,12 @@ module Scenario exposing
 -}
 
 
-
 {-| Main type of this module to represent scenario.
 -}
 type Scenario t v a
-  = Print t (Scenario t v a)
-  | Read (v -> Scenario t v a)
-  | Pure a
+    = Print t (Scenario t v a)
+    | Read (v -> Scenario t v a)
+    | Pure a
 
 
 
@@ -44,34 +44,37 @@ type Scenario t v a
 {-| Construct scenario with any state.
 -}
 succeed : a -> Scenario t v a
-succeed = Pure
+succeed =
+    Pure
 
 
 {-| Combine two scenarios to make one scenario.
 -}
-andThen : Scenario t v a -> (a -> Scenario t v b) -> Scenario t v b
-andThen s f =
-  case s of
-    Print c next ->
-      Print c (next `andThen` f)
+andThen : (a -> Scenario t v b) -> Scenario t v a -> Scenario t v b
+andThen f s =
+    case s of
+        Print c next ->
+            Print c (next |> andThen f)
 
-    Read g ->
-      Read (\v -> (g v `andThen` f))
+        Read g ->
+            Read (\v -> (g v |> andThen f))
 
-    Pure a ->
-      f a
+        Pure a ->
+            f a
 
 
 {-| Similar to `andThen`, but ignores previous state.
 -}
 andAlways : Scenario t v a -> Scenario t v b -> Scenario t v b
-andAlways s1 s2 = s1 `andThen` always s2
+andAlways s1 s2 =
+    s1 |> andThen (always s2)
 
 
 {-| Convert scenario state by given function.
 -}
 map : (a -> b) -> Scenario t v a -> Scenario t v b
-map f m = m `andThen` (succeed << f)
+map f m =
+    m |> andThen (succeed << f)
 
 
 
@@ -81,10 +84,12 @@ map f m = m `andThen` (succeed << f)
 {-| Construct scenario contains only one talk script.
 -}
 print : t -> Scenario t v ()
-print conf = Print conf <| succeed ()
+print conf =
+    Print conf <| succeed ()
 
 
 {-| Construct scenario contains only one choice event.
 -}
 read : Scenario t v v
-read = Read <| succeed
+read =
+    Read <| succeed
