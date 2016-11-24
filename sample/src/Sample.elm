49cc4394f00db0616cfda3a9cb0a89ad632667ed
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-11-24 23:13:14 +0900

    Make sample works

--- sample/src/Sample.elm
+++ sample/src/Sample.elm
@@ -1,13 +1,12 @@
 import Scenario exposing (..)
 
 
-sampleScenario : Scenario msg ()
-sampleScenario =
-  talk <| talkConfig AI Nothing
-    [ { PlainParagraph
-      , "Test"
-      }
-    , { ImportantParagraph
-      , "Is this important question?"
-      }
-    ]
+type alias SimpleScenario a = Scenario () String String a
+
+
+sample : SimpleScenario ()
+sample =
+  print "Test"
+  |> andAlways (print "What's your name?")
+  |> andAlways (read ())
+  |> andThen (\name -> print <| "Hi " ++ name ++ "!")
