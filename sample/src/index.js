958403818d8a8a53ef67fef1e180923ff1670d30
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-11-25 22:14:06 +0900

    Simplify the sample program

--- sample/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// require('./styles/main.scss');
-
-// Inject bundled Elm app into div#main
-var Elm = require('Sample');
-var app = Elm.Main.embed(document.getElementById('main'));
