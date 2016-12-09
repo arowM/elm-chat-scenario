a3997f8a243558dfe4eaa4ab65daa2d378c2eabd
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-12-09 22:24:34 +0900

    Add files for default UI

---
+++ src/Scenario/UI.elm
@@ -0,0 +1,222 @@
+module Scenario.ChatUI exposing
+  ( Model
+  , Config
+  , Msg
+  , update
+  , init
+  , view
+  , config
+  )
+
+{-| A Conversational User Interface component.
+
+# Common Types
+
+@docs Model
+@docs Config
+@docs Msg
+@docs update
+@docs init
+@docs view
+@docs config
+
+-}
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.CssHelpers exposing (Namespace)
+import Html.Events exposing (..)
+import Process as Process
+import Task as Task
+import Time as Time
+
+import Scenario as Scenario
+import Scenario.ChatUI.Css exposing (CssClasses(..))
+
+
+type alias SimpleScenario a = Scenario.Scenario () String String a
+
+
+init : SimpleScenario () -> (Model, Cmd Msg)
+init scenario =
+  ( { scenario = scenario
+    , isReadPhase = False
+    , history = []
+    , input = ""
+    }
+  , Task.perform
+    (always Next)
+    (Task.succeed ())
+  )
+
+
+type alias Model =
+  { scenario : SimpleScenario ()
+  , isReadPhase : Bool
+  , history : List BalloonMessage
+  , input : String
+  }
+
+
+type alias BalloonMessage =
+  { isInput : Bool
+  , message : String
+  }
+
+
+type Msg
+  = Submit String
+  | UpdateInput String
+  | AskRead ()
+  | ShowPrint String
+  | Next
+  | OnEnd
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update message model =
+  case message of
+    Submit str ->
+      update
+        Next
+        { model
+        | scenario = Scenario.pushAnswer str model.scenario
+        , isReadPhase = False
+        , input = ""
+        , history = model.history ++
+          [ { message = str
+            , isInput = True
+            }
+          ]
+        }
+
+    UpdateInput str ->
+      ( { model
+        | input = str
+        }
+      , Cmd.none
+      )
+
+    AskRead () ->
+      ( { model
+        | isReadPhase = True
+        }
+      , Cmd.none
+      )
+
+    ShowPrint str ->
+      update
+        Next
+        { model
+        | history = model.history ++
+          [ { message = str
+            , isInput = False
+            }
+          ]
+        }
+
+    Next ->
+      let
+        (model_, cmd_) = Scenario.update scenarioConfig model.scenario
+      in
+        ( { model | scenario = model_
+          }
+        , cmd_
+        )
+
+    OnEnd ->
+      ( model, Cmd.none )
+
+
+
+-- View
+
+{-| Configuration for view.
+-}
+type Config id = Config
+  { title : String
+  , buttonLabel : String
+  , namespace : Namespace String CssClasses id Msg
+  }
+
+
+{-| A constructor for `Config`.
+-}
+config :
+  { title : String
+  , buttonLabel : String
+  , namespace : Namespace String CssClasses id Msg
+  } -> Config id
+config = Config
+
+
+{-| Default view.
+-}
+view : Config id -> Model -> Html Msg
+view (Config config) model =
+  let { id, class, classList } =
+    config.namespace
+  in
+    div
+      [ class [ Container ]
+      ]
+      [ div
+        [ class [ Header ]
+        ]
+        [ text config.title
+        ]
+      , div
+        [ class [ MessageArea ]
+        ]
+        <| List.map
+          (\msg ->
+            div
+              [ classList
+                [ (Balloon, True)
+                , (IsInput, msg.isInput)
+                ]
+              ]
+              [ text msg.message ]
+          ) model.history
+      , div
+        [ class [ InputArea ] ]
+        [ input
+          [ type_ "text"
+          , value model.input
+          , onInput UpdateInput
+          , class [ SingleInput ]
+          ]
+          []
+        , button
+          [ type_ "button"
+          , disabled <| not model.isReadPhase
+          , onClick <| Submit model.input
+          , class [ SubmitButton ]
+          ]
+          [ text config.buttonLabel
+          ]
+        ]
+      ]
+
+
+
+-- Helper functions
+
+
+scenarioConfig : Scenario.Config Msg () String String
+scenarioConfig = Scenario.config
+  (\t -> Task.perform
+    ShowPrint
+    (Process.sleep
+      (1 * Time.second)
+      |> Task.andThen (always <| Task.succeed t)
+    )
+  )
+  (Task.perform
+    (always OnEnd)
+    (Task.succeed ())
+  )
+  (\c -> Task.perform
+    AskRead
+    (Task.succeed c)
+  )
---
+++ src/Scenario/UI/Css.elm
@@ -0,0 +1,140 @@
+module Scenario.ChatUI.Css exposing
+  ( css
+  , CssClasses(..)
+  )
+
+import Css exposing (..)
+import Css.Namespace exposing (namespace)
+import Html.CssHelpers
+
+
+type CssClasses
+    = Container
+    | Header
+    | MessageArea
+    | Balloon
+    | IsInput
+    | InputArea
+    | SingleInput
+    | SubmitButton
+
+
+css : Html.CssHelpers.Namespace String class id msg -> Stylesheet
+css mynamespace =
+    (stylesheet << namespace mynamespace.name)
+        [ everything
+            [ boxSizing borderBox
+            , fontFamily cursive
+            , padding zero
+            , margin zero
+            , lineHeight (num 1.15)
+            , height auto
+            , width auto
+            , property "border" "none"
+            , textDecoration none
+            , fontWeight normal
+            , fontStyle normal
+            , fontSize (em 1)
+            , boxShadow none
+            , color textColor
+            ]
+        , (.) Container
+            [ displayFlex
+            , flexDirection column
+            , height (vh 100)
+            , minHeight (pct 100)
+            , maxWidth (em 60)
+            , margin2 zero auto
+            ]
+        , (.) Header
+            [ width (pct 100)
+            , minHeight (em 2)
+            , backgroundColor mainColor
+            , boxShadow5 zero (px 4) (px 4) (px -4) darkColor
+            , fontSize (em 1.4)
+            , color white
+            , textAlign center
+            , padding3 (em 0.5) zero (em 0.5)
+            ]
+        , (.) MessageArea
+            [ flex (int 1)
+            , overflowY scroll
+            , children
+              [ (.) Balloon
+                [ border3 (px 2.4) solid mainColor
+                , borderRadius (px 4)
+                , margin3 zero zero (em 0.5)
+                , position relative
+                , lineHeight (em 2)
+                , color textColor
+                , textAlign left
+                , backgroundColor lightMainColor
+                , fontSize (em 1.2)
+                , margin2 (em 0.4) (em 0.4)
+                , padding2 (em 0.2) (em 0.4)
+                ]
+              , (.) IsInput
+                [ backgroundColor lightPrimaryColor
+                , borderColor primaryColor
+                ]
+              ]
+            ]
+        , (.) InputArea
+            [ width (pct 100)
+            , minHeight (em 2)
+            , backgroundColor mainColor
+            , boxShadow5 zero (px -4) (px -4) (px 4) darkColor
+            , fontSize (em 1.4)
+            , color white
+            , textAlign right
+            , padding3 (em 0.5) (em 0.5) (em 0.5)
+            , children
+              [ everything
+                [ fontSize (em 1.1)
+                , height (em 1.6)
+                ]
+              , (.) SingleInput
+                [ borderRadius (px 6)
+                , width (pct 100)
+                , maxWidth (em 20)
+                , padding2 zero (em 0.4)
+                ]
+              , (.) SubmitButton
+                [ borderRadius (px 6)
+                , minWidth (em 6)
+                , marginLeft (em 1.4)
+                , backgroundColor primaryColor
+                , cursor pointer
+                , hover
+                  [ backgroundColor lightPrimaryColor
+                  ]
+                , disabled
+                  [ backgroundColor darkPrimaryColor
+                  , color white
+                  , cursor default
+                  ]
+                ]
+              ]
+            ]
+        ]
+
+lightMainColor : Color
+lightMainColor = hex "aee7ff"
+mainColor : Color
+mainColor = hex "5ea7e1"
+textColor : Color
+textColor = hex "1b5a74"
+primaryColor : Color
+primaryColor = hex "94cf85"
+darkPrimaryColor : Color
+darkPrimaryColor = hex "7caa70"
+lightPrimaryColor : Color
+lightPrimaryColor = hex "aade9d"
+secondaryColor : Color
+secondaryColor = hex "dec575"
+darkColor : Color
+darkColor = hex "3c85bf"
+white : Color
+white = hex "fff"
+black : Color
+black = hex "000"
