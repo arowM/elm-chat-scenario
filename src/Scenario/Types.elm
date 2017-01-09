bfaaf4be566aeaf8dbd77c6c1d82c232acdcbc24
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-01-09 17:59:45 +0900

    Add Scenario.Types for shared types

---
+++ src/Scenario/Types.elm
@@ -0,0 +1,49 @@
+module Scenario.Types
+  exposing
+    ( ReadValue
+    , ImageSrc
+    , Label
+    , Name
+    , Namespace
+    )
+
+{-| Shared types for CUI.
+
+# Aliases
+
+@docs ReadValue
+@docs ImageSrc
+@docs Label
+@docs Name
+@docs Namespace
+-}
+
+
+{-| An alias of `String` for readability.
+-}
+type alias ReadValue =
+  String
+
+
+{-| An alias of `String` for readability.
+-}
+type alias ImageSrc =
+  String
+
+
+{-| An alias of `String` for readability.
+-}
+type alias Label =
+  String
+
+
+{-| An alias of `String` for readability.
+-}
+type alias Name =
+  String
+
+
+{-| An alias for `String` representing css namespace.
+-}
+type alias Namespace =
+  String
